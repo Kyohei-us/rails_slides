fe5694c192974770dd2ce2bd4951895aab2d93a5
Kyohei-us <kyohei1212000@gmail.com>
2022-01-20 07:36:59 -0800

    add spotify preview handler function

--- app/helpers/music_tracks_helper.rb
+++ app/helpers/music_tracks_helper.rb
@@ -1,2 +1,11 @@
 module MusicTracksHelper
+    def handleSpotifyPreview(music_track)
+        preview = ""
+        if music_track.spotifyURI == ""
+            preview = "<p>Preview Not Available for #{music_track.name}</p>"
+        else
+            preview = "<iframe class='spotify_compact preview_player' src=#{spotifyURItoEmbed(music_track.spotifyURI)} width='300' frameborder='0' allowtransparency='true' allow='encrypted-media'></iframe>"
+        end
+        return preview.html_safe
+    end
 end
